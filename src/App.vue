63998beb64796f02f8ab1cadbe7f7b17188a78c5
rivalcoba <ivan.rivalcoba@gmail.com>
2023-01-25 19:50:55 -0600

    ⭐️ Log enviroment

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from "@/components/HelloWorld.vue";
+
+// Accediendo a las variables de entorno
+console.log(import.meta.env);
 </script>
 
 <template>
